c675b5a07767be117b6c939913037367f7e73f83
MERSEL Dali <dali.mersel@gmail.com>
2022-12-05 16:59:25 +0100

    Init project

---
+++ segmentation.py
@@ -0,0 +1,34 @@
+import io
+
+import tensorflow as tf
+
+from PIL import Image
+
+from . import cityscapes
+
+
+
+
+def get_segmentator():
+
+    model = tf.keras.models.load_model('models/best_model.h5')
+
+    return model
+
+
+def get_segments(model, binary_image, max_size=1024):
+
+    input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+    resized_image = input_image.resize((1024,1024))
+
+    categories_img = Image.fromarray(
+        cityscapes.cityscapes_category_ids_to_category_colors(
+            np.squeeze(
+                np.argmax(
+                    model.predict(np.expand_dims(input_image, 0)), axis=-1
+                )
+            )
+        )
+    )
+
+    return categories_img
