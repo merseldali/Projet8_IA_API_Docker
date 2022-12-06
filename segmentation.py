c9a91f217503b57dac01ee7099edbd0d3499cb47
MERSEL Dali <dali.mersel@gmail.com>
2022-12-06 22:38:05 +0100

    add of image id support

--- segmentation.py
+++ segmentation.py
@@ -1,14 +1,8 @@
 import io
-
 import tensorflow as tf
-
 from PIL import Image
-
 import cityscapes
-
 import numpy as np
-
-
 
 
 def get_segmentator():
@@ -34,3 +28,45 @@ def get_segments(model, binary_image, max_size=1024):
     )
 
     return categories_img
+    
+def get_segments_by_id(model, image_id):
+    
+    leftImg8bit_path = Path("data", "images")
+    gtFine_path = Path("data", "labels")
+
+    image_id = str(image_id)
+    input_img_paths = []
+    labels_img_paths = []
+    if image_id:
+        input_img_paths = sorted(
+            Path(leftImg8bit_path).glob(f"**/*{image_id}*_leftImg8bit.png")
+        )
+        labels_img_paths = sorted(
+            Path(gtFine_path).glob(f"**/*{image_id}*_color.png")
+        )
+    if (len(input_img_paths) == 0 or len(labels_img_paths) == 0):
+        return None
+        
+    with open(input_img_paths[0], "rb") as f:
+        original_img_b64 = base64.b64encode(f.read())
+        original_img_b64_str = original_img_b64.decode("utf-8")
+
+        input_img = Image.open(
+            BytesIO(base64.b64decode(original_img_b64))
+        ).convert("RGB").resize(img_size)
+        categories_img = Image.fromarray(
+            cityscapes.cityscapes_category_ids_to_category_colors(
+                np.squeeze(
+                    np.argmax(
+                        model.predict(np.expand_dims(input_img, 0)), axis=-1
+                    )
+                )
+            )
+        )
+    
+    with open(labels_img_paths[0], "rb") as f:
+        labels_img_read = f.read()
+        labels_img_b64 = base64.b64encode(labels_img_read)
+        label_img = Image.open(labels_img_b64).convert("RGB")
+
+    return categories_img, label_img
--- server.py
+++ server.py
@@ -17,3 +17,54 @@ def get_segmentation_map(file: bytes = File(...)):
     bytes_io = io.BytesIO()
     segmented_image.save(bytes_io, format='PNG')
     return Response(bytes_io.getvalue(), media_type="image/png")
+    
+    
+@app.post("/segmentation/{image_id}")
+def get_segmentation_map_by_id(image_id: int):
+    segmented_image, label_image = get_segments_by_id(model, image_id)
+    if (segmented_image != None) and (label_image != None):
+        bytes_io = io.BytesIO()
+        bytes_io2 = io.BytesIO()
+        segmented_image.save(bytes_io, format='PNG')
+        label_image.save(bytes_io2, format='PNG')
+    
+        # Open StringIO to grab in-memory ZIP contents
+        s = StringIO.StringIO()
+        # The zip compressor
+        zf = zipfile.ZipFile(s, "w")
+    
+        zip_file.writestr('predicted.png', bytes_io.getvalue())
+        zip_file.writestr('labels.png', bytes_io2.getvalue())
+    
+        zip_file.close()
+    
+        return Response(s.getvalue(), media_type="application/x-zip-compressed")
+    else:
+        return None
+  
+def zipfiles(filenames):
+    zip_subdir = "archive"
+    zip_filename = "%s.zip" % zip_subdir
+
+    # Open StringIO to grab in-memory ZIP contents
+    s = StringIO.StringIO()
+    # The zip compressor
+    zf = zipfile.ZipFile(s, "w")
+
+    for fpath in filenames:
+        # Calculate path for file in zip
+        fdir, fname = os.path.split(fpath)
+        zip_path = os.path.join(zip_subdir, fname)
+
+        # Add file, at correct path
+        zf.write(fpath, zip_path)
+
+    # Must close zip for all contents to be written
+    zf.close()
+
+    # Grab ZIP file from in-memory, make response with correct MIME-type
+    resp = Response(s.getvalue(), mimetype = "application/x-zip-compressed")
+    # ..and correct content-disposition
+    resp['Content-Disposition'] = 'attachment; filename=%s' % zip_filename
+
+    return resp
