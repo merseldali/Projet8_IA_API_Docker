40100c2af687e30d7647d8ed16421c45e617c34c
MERSEL Dali <dali.mersel@gmail.com>
2022-12-07 20:27:41 +0100

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -67,7 +67,7 @@ def get_segments_by_id(model, image_id):
     
     with open(labels_img_paths[0], "rb") as f:
         labels_img_read = f.read()
-        labels_img_b64 = base64.b64encode(labels_img_read)
-        label_img = Image.open(labels_img_b64).convert("RGB")
+        #labels_img_b64 = base64.b64encode(labels_img_read)
+        label_img = Image.open(labels_img_read).convert("RGB")
 
     return categories_img, label_img
